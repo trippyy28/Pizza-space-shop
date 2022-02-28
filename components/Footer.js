5ef9a1edd5385fb361c5831637ba4b20cbe0769f
user <user@users-MacBook-Pro.local>
2022-02-28 15:08:51 +0200

    init commit

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,11 @@
-import React from 'react'
-
+import React from "react";
+import styles from "../styles/Footer.module.css";
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className={styles.container}>
+      <h1>All the rights reserved</h1>
+    </div>
+  );
 }
 
 export default Footer;
--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Footer from './Footer'
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
-function Layout({children}) {
+function Layout({ children }) {
   return (
     <>
-     <Navbar/>
+      <Navbar />
       {children}
-      <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
 
 export default Layout;
